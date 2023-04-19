72443bbdc2726f4a29e187ed652eb97d744d59a3
Amit Kumar Gupta <113920422+Amit-IITM@users.noreply.github.com>
2023-04-19 22:45:23 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+def find_largest(a,b,c):
+    if a >= b and a >= c:
+        return a
+    elif b >= a and b >= c:
+        return b
+    else:
+        return c
+
+st.title("This is your Largest Number")
+
+a = st.number_input("First number:")
+b = st.number_input("Second number:")
+c = st.number_input("Third number:")
+
+if st.button("Largest Number : "):
+    largest_num = find_largest(a,b,c)
+    st.write(f"The largest number : {largest_num}")
